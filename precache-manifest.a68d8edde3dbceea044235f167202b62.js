728110e763b1f25e6dcd0604fe19510825785419
peterzhang <raben4@hotmail.com>
2020-07-11 14:45:37 +1000

    Updates

---
+++ precache-manifest.a68d8edde3dbceea044235f167202b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81bf65add4b8c04c24c0ebd787595d5",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "0ccc31853bad42ac2216",
+    "url": "/pokemon/static/css/main.2841055f.chunk.css"
+  },
+  {
+    "revision": "912988a4f54d0442b257",
+    "url": "/pokemon/static/js/2.e48b6de5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.e48b6de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ccc31853bad42ac2216",
+    "url": "/pokemon/static/js/main.79bc6243.chunk.js"
+  },
+  {
+    "revision": "b931b09f9078cae05f94",
+    "url": "/pokemon/static/js/runtime-main.89aedc36.js"
+  }
+]);
